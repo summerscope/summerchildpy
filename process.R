f7a944010a7d9f1ebaf7d5f0c4dd3aa2dba82adb
Evie <mezigreke@gmail.com>
2022-02-22 22:48:50 +1100

    added json read in option and created input type

--- process.R
+++ process.R
@@ -2,7 +2,17 @@ library(dplyr)
 library(tidyr)
 library(magrittr)
 library(readr)
+library(stringr)
+library(shinysurveys)
+library(jsonlite)
 
+# Give the input file name to the function.
+# If you wanna read it from json and not csv
+# you can run this code and wrangle it from there! :)
+
+survey_input <- jsonlite::fromJSON(txt = "questions.json", 
+                                   flatten = TRUE, 
+                                   simplifyDataFrame = TRUE  )
 
 quest = read_csv("questions.csv")
 sub <- quest %>% 
@@ -14,4 +24,5 @@ sub_long <- sub %>%
                cols = answers.A.text:answers.G.text,
                names_to = "option_name",
                values_to = "option") %>%
-  rename(question = text)
+  rename(question = text) %>%
+  mutate(input_type = ifelse(str_detect(question, "Section"), "select", "mc"))
