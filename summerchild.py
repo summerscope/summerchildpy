1783645f15b36d1d2891df095698d8905580d932
Laura Summers <summerscope@gmail.com>
2022-02-27 15:35:41 +1100

    Get basic logic loop working

--- summerchild.py
+++ summerchild.py
@@ -13,23 +13,24 @@ State = collections.namedtuple('State', ['multiplier', 'score', 'currentq', 'rec
 
 start_state = State(multiplier = 1, score = 0, currentq = "Q1", recommendations = [])
 
-def print_question(text, answers, answer_set, score):
+def print_question(text, answers, answer_set):
   print(text)
   print(answers)
   x = input()
   uppercase_input = x.upper()
-  if uppercase_input in answer_set:
-    print('You selected ' + x)    
-    return uppercase_input
-  else:
-    print('Sorry '+ x +' is not a valid answer, please try again')
-    return None
+  for answer in answer_set:
+    if answer.upper() == uppercase_input:
+      print('You selected ' + x)    
+      return answer
+  # This is what happens if input is invalid
+  print('\n⚠️  Sorry '+ x +' is not a valid answer, please try again  ⚠️\n')
+  return None
 
-def validate_answer(text, answers, answer_set, score):
+def validate_answer(text, answers, answer_set):
   """The purpose of this function is to keep asking the question until we get a valid answer"""
   answered = None
   while not answered:
-    answered = print_question(text, answers, answer_set, score)
+    answered = print_question(text, answers, answer_set)
   return answered
 
 def format_question(question):
@@ -41,7 +42,7 @@ def format_question(question):
   answers_joined = "\n".join(answer_list)
   # print (answers_joined)
   # print (text)
-  answer_key = validate_answer(text, answers_joined, answers.keys(), 0)
+  answer_key = validate_answer(text, answers_joined, answers.keys())
   return answers[answer_key]
 
 def update_state(state, answer):
@@ -58,11 +59,28 @@ def ask_question(question, state):
   new_state = update_state(state, answer)
   return new_state
 
+def lookup_question(data, id):
+  for question in data: 
+    if question["id"] == id:
+      return question
+  raise KeyError(id + "was not found")
+
+def print_summary(state):
+  print(state)
+
+def run_quiz(data, state):
+  while state.currentq and state.currentq != "":
+    question = lookup_question(data, state.currentq)
+    state = ask_question(question, state)
+  print_summary(state)
+
 # format_question(data[1])
 
-print(start_state)
-next_state = ask_question(data[0], start_state)
-print(next_state)
+run_quiz(data, start_state._replace(currentq = "Q18"))
+
+# print(start_state)
+# next_state = ask_question(data[0], start_state)
+# print(next_state)
 
 
 
