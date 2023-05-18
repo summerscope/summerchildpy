be5221bae39cf2626415696a5b4a6c07fec31af8
Andy Kitchen <kitchen.andy@gmail.com>
2023-05-18 19:03:57 +0200

    switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="summerchild_quiz",
-    version="0.0.1",
-    author="Laura Summers",
-    author_email="summerscope@gmail.com",
-    description="Sweet summer child score, a risk assessment quiz for automation and ML.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/summerscope/summerchildpy",
-    project_urls={
-        "Bug Tracker": "https://github.com/summerscope/summerchildpy/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "."},
-    packages=['summerchild'],
-    package_data={
-        "summerchild": ["questions.json"],
-    },
-    python_requires=">=3.7",
-    entry_points={
-        'console_scripts': ['summerchild_quiz = summerchild:main']
-    },
-)
+setup()
